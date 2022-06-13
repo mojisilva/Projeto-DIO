16c5f543ba55758a607f344b87b503e0f46cbdb9
mojisilva <mojiasilva@gmail.com>
2022-06-13 14:36:58 -0300

    update README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto DIO TypeScript
+
+Repositório dos desafios de código propostos na aula de typescript DIO.
---
+++ src/desafio2.ts
@@ -0,0 +1,36 @@
+// Como podemos melhorar o esse código usando TS? 
+
+interface Pessoa{
+    nome:string,
+    idade:number,
+    profissao: Profissao,
+}
+
+enum Profissao{
+    Atriz,
+    Padeiro
+}
+
+let pessoa1: Pessoa = {
+    nome:"Maria",
+    idade: 29,
+    profissao: Profissao.Atriz,
+}
+
+let pessoa2: Pessoa = {
+    nome: "Roberto",
+    idade: 19,
+    profissao: Profissao.Padeiro,
+}
+
+let pessoa3: Pessoa = {
+    nome: "Laura",
+    idade: 32,
+    profissao: Profissao.Atriz,
+}
+
+let pessoa4:Pessoa = {
+    nome: "Carlos",
+    idade: 19,
+    profissao: Profissao.Padeiro,
+}
